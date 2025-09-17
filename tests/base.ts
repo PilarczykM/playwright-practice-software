b8f67f1e62044d785aba93693786d4225c7d4e08
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-09-17 13:05:01 +0200

    feat: add network error monitoring to tests

---
+++ tests/base.ts
@@ -0,0 +1,40 @@
+import { test as base, type Page } from "@playwright/test";
+
+interface ErrorRequest {
+  url: string;
+  status: number;
+}
+
+export const test = base.extend<{ networkErrorMonitoring: Page }>({
+  networkErrorMonitoring: async ({ page }, use, testInfo) => {
+    const errorData: ErrorRequest[] = [];
+    page.on("response", async (response) => {
+      const url = response.url();
+      const status = response.status();
+
+      if (status >= 400) {
+        const errorRequest: ErrorRequest = {
+          url,
+          status,
+        };
+
+        errorData.push(errorRequest);
+      }
+    });
+
+    await use(page);
+
+    if (errorData.length > 0) {
+      const fileName = "error_requests.json";
+      await testInfo.attach(fileName, {
+        body: JSON.stringify(errorData, null, 2),
+        contentType: "application/json",
+      });
+      throw new Error(
+        `Network errors detected: ${errorData.length} requests failed. Check the attached ${fileName} file.`
+      );
+    }
+  },
+});
+
+export { expect } from "@playwright/test";
