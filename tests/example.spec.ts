a2de48616db51ced62e742dff877a14e7e6e8af7
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-09-17 13:08:21 +0200

    Code lint

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,22 +4,22 @@ import { expect, test } from "./base";
 let homePage: HomePage;
 
 test.describe("Home page spec", () => {
-  test.beforeEach(async ({ page }) => {
-    homePage = new HomePage(page);
-    await homePage.goto();
-  });
+	test.beforeEach(async ({ page }) => {
+		homePage = new HomePage(page);
+		await homePage.goto();
+	});
 
-  test("check page title", async ({ page }) => {
-    await expect(page).toHaveTitle(
-      "Practice Software Testing - Toolshop - v5.0"
-    );
-  });
+	test("check page title", async ({ page }) => {
+		await expect(page).toHaveTitle(
+			"Practice Software Testing - Toolshop - v5.0",
+		);
+	});
 });
 
 test.fail(
-  "Homepage network monitoring",
-  async ({ networkErrorMonitoring: page }) => {
-    homePage = new HomePage(page);
-    await homePage.goto();
-  }
+	"Homepage network monitoring",
+	async ({ networkErrorMonitoring: page }) => {
+		homePage = new HomePage(page);
+		await homePage.goto();
+	},
 );
