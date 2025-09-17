df9c2da985552f17b92959340a9f96e86a372145
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-09-17 23:20:37 +0200

    refactor: introduce fixtures for better test setup
    Refactored the Playwright test setup to use fixtures for better organization and reusability.
    
    - Introduced custom fixtures for logging, network monitoring, and page object initialization.
    - Moved Page Object Models (POMs) to a dedicated `tests/poms` directory.
    - Updated test specs to use the new fixtures, resulting in cleaner and more maintainable code.

--- lib/pages/Home.ts
+++
@@ -1,9 +0,0 @@
-import type { Page } from "@playwright/test";
-
-export class HomePage {
-	constructor(private readonly page: Page) {}
-
-	async goto() {
-		await this.page.goto("/");
-	}
-}
---
+++ tests/fixtures/base.ts
@@ -0,0 +1,82 @@
+import { test as base, type Page } from "@playwright/test";
+
+interface ErrorRequest {
+  url: string;
+  status: number;
+}
+
+export const test = base.extend<{
+  timeLogger: Promise<void>;
+  errorLogger: Promise<void>;
+  networkErrorMonitoring: Page;
+}>({
+  // Custom timeLogger fixture with annotation.
+  // It can be user type, or some different information for specific tests.
+  timeLogger: [
+    async ({ page }, use, testInfo) => {
+      testInfo.annotations.push({
+        type: "Start",
+        description: new Date().toISOString(),
+      });
+
+      await use(page);
+
+      testInfo.annotations.push({
+        type: "End",
+        description: new Date().toISOString(),
+      });
+    },
+    { auto: true },
+  ],
+  errorLogger: [
+    async ({ page }, use) => {
+      const errors: Error[] = [];
+
+      page.on("pageerror", (err) => {
+        errors.push(err);
+      });
+
+      await use(page);
+
+      if (errors.length > 0) {
+        await test.info().attach("frontend-exceptions", {
+          body: errors
+            .map((error) => `${error.message}\n${error.stack}`)
+            .join("\n----\n"),
+        });
+      }
+    },
+    { auto: true },
+  ],
+  networkErrorMonitoring: async ({ page }, use, testInfo) => {
+    const errorData: ErrorRequest[] = [];
+    page.on("response", async (response) => {
+      const url = response.url();
+      const status = response.status();
+
+      if (status >= 400) {
+        const errorRequest: ErrorRequest = {
+          url,
+          status,
+        };
+
+        errorData.push(errorRequest);
+      }
+    });
+
+    await use(page);
+
+    if (errorData.length > 0) {
+      const fileName = "error_requests.json";
+      await testInfo.attach(fileName, {
+        body: JSON.stringify(errorData, null, 2),
+        contentType: "application/json",
+      });
+      throw new Error(
+        `Network errors detected: ${errorData.length} requests failed. Check the attached ${fileName} file.`
+      );
+    }
+  },
+});
+
+export { expect } from "@playwright/test";
